4537d46b92effacbfd5fd26b2b7655c6ff60a7c1
Benyamain Yacoob <yacoobbenyamain@gmail.com>
2023-11-16 08:08:30 -0500

    trained on both datasets

--- trapdoor/inject_trapdoor.py
+++ trapdoor/inject_trapdoor.py
@@ -128,15 +128,11 @@ def main():
 
     print("First Step: Training Normal Model...")
     new_model.fit_generator(clean_train_gen, validation_data=test_nor_gen, steps_per_epoch=number_images // 32,
-                            epochs=model.epochs, verbose=2, callbacks=callbacks, validation_steps=100,
-                            use_multiprocessing=True,
-                            workers=1)
+                            epochs=model.epochs, verbose=2, callbacks=callbacks, validation_steps=100)
 
     print("Second Step: Injecting Trapdoor...")
     new_model.fit_generator(trap_train_gen, validation_data=test_nor_gen, steps_per_epoch=number_images // 32,
-                            epochs=model.epochs, verbose=2, callbacks=callbacks, validation_steps=100,
-                            use_multiprocessing=True,
-                            workers=1)
+                            epochs=model.epochs, verbose=2, callbacks=callbacks, validation_steps=100)
 
     if not os.path.exists(model_file):
         raise Exception("NO GOOD MODEL!!!")
